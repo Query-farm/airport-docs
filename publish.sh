e4e3f07a8aa4fd518c16c95d573e8ac6838cc28d
Rusty Conover <rusty@conover.me>
2025-12-01 11:47:09 -0500

    fix: improve docs

--- publish.sh
+++ publish.sh
@@ -2,6 +2,6 @@
 set -ex
 source ./.venv/bin/activate
 
-quarto render
+/Users/rusty/Development/quarto-cli/package/dist/bin/quarto render
 cd _site
 aws s3 sync --delete --cache-control "public, max-age=15" . s3://query-farm-airport-docs/
